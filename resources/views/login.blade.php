f7d487988086959736683ea90771596c5b8caa0d
rizkiabdillahazmi <rizkiabdillahazmi10@gmail.com>
2022-03-26 00:16:53 +0700

    update login and register blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
             </svg> --}}
         </div>
-        <h3 class="text-2xl font-bold text-center">Login to your account</h3>
+        <h3 class="text-2xl font-bold text-center">Silahkan Login</h3>
         <form action="/login" method="post">
             @csrf
             <div class="mt-4 w-96">
@@ -27,7 +27,7 @@
                 </div>
                 <div class="flex items-baseline justify-between">
                     <button class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</button>
-                    <a href="#" class="text-sm text-blue-600 hover:underline">Forgot password?</a>
+                    <a href="#" class="text-sm text-blue-600 hover:underline">Lupa Password?</a>
                 </div>
             </div>
         </form>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,12 +9,12 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
             </svg> --}}
         </div>
-        <h3 class="text-2xl font-bold text-center">Join us</h3>
+        <h3 class="text-2xl font-bold text-center">Daftar Sekarang</h3>
         <form action="">
             <div class="mt-4">
                 <div>
-                    <label class="block" for="Name">Name<label>
-                            <input type="text" placeholder="Name" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+                    <label class="block" for="Name">Nama<label>
+                            <input type="text" placeholder="Nama" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
                 </div>
                 <div class="mt-4">
                     <label class="block" for="email">Email<label>
@@ -25,16 +25,15 @@
                             <input type="password" placeholder="Password" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
                 </div>
                 <div class="mt-4">
-                    <label class="block">Confirm Password<label>
+                    <label class="block">Konfirmasi Password<label>
                             <input type="password" placeholder="Password" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
                 </div>
-                <span class="text-xs text-red-400">Password must be same!</span>
+                <span class="text-xs text-red-400">Password harus sama!</span>
                 <div class="flex">
-                    <button class="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Create
-                        Account</button>
+                    <button class="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Buat Akun</button>
                 </div>
                 <div class="mt-6 text-grey-dark">
-                    Already have an account?
+                    Sudah Punya Akun?
                     <a class="text-blue-600 hover:underline" href="/login">
                         Log in
                     </a>
